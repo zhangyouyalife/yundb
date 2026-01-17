4f201111bc2c5de18db6c26dbdab4272c74aa613
zhangyouyalife <zhang.youya.life@gmail.com>
2026-01-17 11:46:01 +0800

    Insert and Delete, but List will output deleted records

--- fo_fixed.c
+++ fo_fixed.c
@@ -22,14 +22,23 @@ struct __attribute__((packed)) RecordPointer
     uint16_t  off;
 };
 
-struct __attribute__((packed)) FileHeader
-{
+struct __attribute__((packed)) FileHeader {
     struct RecordPointer last;
+    struct RecordPointer free;
+};
+
+struct __attribute__((packed)) FreeNode {
+    struct RecordPointer next;
 };
 
 #define DB_FILE_NAME    "fo_fixed.db"
 int file_fd;
 struct FileHeader file_header;
+
+void RecordPointerSet(struct RecordPointer *p, uint32_t bno, uint16_t off) {
+    p->bno = bno;
+    p->off = off;
+}
 
 void FileCreate()
 {
@@ -43,8 +52,8 @@ void FileCreate()
     }
 
     h = (struct FileHeader *) block;
-    h->last.bno = 0;
-    h->last.off = sizeof(struct FileHeader);
+    RecordPointerSet(&(h->last), 0, sizeof(struct FileHeader));
+    RecordPointerSet(&(h->free), 0, 0);
     
     BlockWrite(dbf, 0, block);
 
@@ -68,8 +77,8 @@ void FileOpen()
     BlockRead(file_fd, 0, block);
 
     p = (struct FileHeader *) block;
-    file_header.last.bno = p->last.bno;
-    file_header.last.off = p->last.off;
+    RecordPointerSet(&file_header.last, p->last.bno, p->last.off);
+    RecordPointerSet(&file_header.free, p->free.bno, p->free.off);
 }
 
 void FileClose()
@@ -79,8 +88,8 @@ void FileClose()
     BlockRead(file_fd, 0, block);
 
     p = (struct FileHeader *) block;
-    p->last.bno = file_header.last.bno;
-    p->last.off = file_header.last.off;
+    RecordPointerSet(&p->last, file_header.last.bno, file_header.last.off);
+    RecordPointerSet(&p->free, file_header.free.bno, file_header.free.off);
 
     BlockWrite(file_fd, 0, block);
 
@@ -138,6 +147,8 @@ struct __attribute__((packed)) instructor
     double salary;
 };
 
+void RecordAppend(struct instructor *ins);
+
 void output_record(struct instructor *ins);
 
 void usage()
@@ -191,6 +202,48 @@ void input_double(double *d)
     scanf("%lf", d);
 }
 
+void RecordDelete(struct RecordPointer *p)
+{
+    struct FreeNode *n;
+
+    BlockRead(file_fd, p->bno, block);
+
+    n = (struct FreeNode *) (block + p->off);
+    RecordPointerSet(&n->next, 
+            file_header.free.bno,
+            file_header.free.off);
+    RecordPointerSet(&(file_header.free),
+            p->bno, p->off);
+
+    BlockWrite(file_fd, p->bno, block);
+}
+
+void RecordInsert(struct instructor *ins)
+{
+    struct FreeNode *fp;
+    int bno;
+
+    if (file_header.free.bno == 0 && file_header.free.off == 0)
+    {
+        RecordAppend(ins);
+    }
+    else
+    {
+        // free node
+        bno = file_header.free.bno;
+
+        BlockRead(file_fd, bno, block);
+       
+        fp = (struct FreeNode *) (block + file_header.free.off);
+
+        RecordPointerSet(&file_header.free, fp->next.bno, fp->next.off); 
+
+        memcpy(fp, ins, RECORD_SZ);
+
+        BlockWrite(file_fd, bno, block);
+    }
+}
+
 void RecordAppend(struct instructor *ins)
 {
     int off, bno;
@@ -218,6 +271,21 @@ void RecordAppend(struct instructor *ins)
     file_header.last.off = off + RECORD_SZ;
 }
 
+void RecordInput(struct instructor *r)
+{
+    printf("Id: ");
+    input_str(r->id, 5);
+
+    printf("Name: ");
+    input_str(r->name, 20);
+
+    printf("Dept Name: ");
+    input_str(r->dept_name, 20);
+
+    printf("Salary: ");
+    input_double(&r->salary);
+}
+
 void append_record(int dbf)
 {
     struct instructor ins;
@@ -237,7 +305,7 @@ void append_record(int dbf)
 
     //rsize = sizeof(ins);
     //printf("%d", rsize);
-    puts("Writing record...");
+    puts("Appending record...");
     RecordAppend(&ins);
 }
 
@@ -252,6 +320,16 @@ void output_record(struct instructor *ins)
     printf("\nSalary %lf\n", ins->salary);
 }
 
+void insert_record()
+{
+    struct instructor ins;
+
+    RecordInput(&ins);
+
+    puts("Inserting record ...");
+
+    RecordInsert(&ins);
+}
 
 void list_records(int dbf)
 {
@@ -268,8 +346,10 @@ void list_records(int dbf)
                 || off + RECORD_SZ <= file_header.last.off)
         {
             ins = (struct instructor *) (block + off);
-            output_record(ins);
+            puts("===");
+            printf("Record (%u, %u)\n", bno, off);
             puts("---");
+            output_record(ins);
             off += RECORD_SZ;
         }
 
@@ -282,14 +362,17 @@ void list_records(int dbf)
 #define OP_APPEND       1
 #define OP_LIST         2
 #define OP_CREATEFILE   3
+#define OP_INSERT       4
+#define OP_DELETE       5
 
 int main(int argc, char** argv)
 {
     int dbf;
     int ch;
     int op;
+    struct RecordPointer rp;
 
-    while ((ch = getopt(argc, argv, "fal")) != -1)
+    while ((ch = getopt(argc, argv, "faidl")) != -1)
     {
         switch (ch) {
             case 'f':
@@ -297,6 +380,12 @@ int main(int argc, char** argv)
                 break;
             case 'a':
                 op = OP_APPEND;
+                break;
+            case 'i':
+                op = OP_INSERT;
+                break;
+            case 'd':
+                op = OP_DELETE;
                 break;
             case 'l':
                 op = OP_LIST;
@@ -328,6 +417,15 @@ int main(int argc, char** argv)
             break;
         case OP_LIST:
             list_records(dbf);
+            break;
+        case OP_INSERT:
+            insert_record();
+            break;
+        case OP_DELETE:
+            rp.bno = atoi(argv[0]);
+            rp.off = atoi(argv[1]);
+            printf("Deleteing record at %u %u\n", rp.bno, rp.off);
+            RecordDelete(&rp);
             break;
         default:
             puts("Invalid operation\n");
